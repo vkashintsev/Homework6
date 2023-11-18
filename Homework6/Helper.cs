a07799f2e2b126312788643d3be2db9ea53c1b83
Vladimir Kashintsev <kashintsevvs@gmail.com>
2023-11-18 18:26:08 +0300

    Add project files.

---
+++ Homework6/Helper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+    public class Helper
+    {
+        public async Task<int> ReadFileAsync(String fileName)
+        {
+            int spaces = 0;
+            using (var sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    spaces += line.Count(Char.IsWhiteSpace);
+                }
+            }
+            return spaces;
+        }
+
+        public int[] ReadDirectoryAsync(String directoryName)
+        {
+            var files = Directory.GetFiles(directoryName);
+            var tasks = new List<Task<int>>();
+            foreach (var file in files)
+            {
+                tasks.Add(ReadFileAsync(file));
+            }
+            return Task.WhenAll(tasks).Result; 
+        }
+          
+    }
+}
---
+++ Homework6/Program.cs
@@ -0,0 +1,49 @@
+﻿using Homework6;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Прочитать 3 файла параллельно и вычислить количество пробелов в них (через Task)."); 
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+        var res1 = Task1(@"D:\Test\File1.txt", @"D:\Test\File2.txt", @"D:\Test\File3.txt");
+        stopWatch.Stop();
+        foreach (var task in res1)
+        {
+            Console.WriteLine(task);
+        }
+        Console.WriteLine($"Время: {stopWatch.ElapsedMilliseconds}");
+
+
+        Console.WriteLine("Написать функцию, принимающую в качестве аргумента путь к папке. Из этой папки параллельно прочитать все файлы и вычислить количество пробелов в них");
+        stopWatch.Reset();
+        stopWatch.Start();
+        var res2 = Task2();
+        stopWatch.Stop();
+        foreach (var task in res2)
+        {
+            Console.WriteLine(task);
+        }
+        Console.WriteLine($"Время: {stopWatch.ElapsedMilliseconds}");
+        Console.ReadKey(); 
+    }
+    private static int[] Task1(String fileName1, String fileName2, String fileName3)
+    {
+        var task = new Helper();
+        var file1 = task.ReadFileAsync(fileName1);
+        var file2 = task.ReadFileAsync(fileName2);
+        var file3 = task.ReadFileAsync(fileName3); 
+        return Task.WhenAll(file1, file2, file3).Result;
+    }
+
+    private static int[] Task2()
+    { 
+        var task = new Helper();
+        return task.ReadDirectoryAsync("D:\\Test");
+    }
+
+
+    }
